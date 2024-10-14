a3d5254490b88f08ad22a75dd8172c4d6eee47fd
logunov.a <logunov.a@ati.su>
2024-10-14 11:00:39 +0300

    some refactor

--- src/modules/damage_module.py
+++ src/modules/damage_module.py
@@ -2,4 +2,4 @@ from math import log2
 
 
 def multiplier(damage):
-    return f'Множитель урона x{damage} даст {round(log2(damage) * 11, 2)} дней'
+    return f'Множитель урона x{damage} даст {round(log2(damage) * 11)} дней'
